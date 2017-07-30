f078ea07a934c4ac03b0d3783ee23f64ab4580fb
Daniel Tjandra <danieltjandra@gmail.com>
2017-07-30 17:54:19 -0400

    Make it easy to switch config file and log file location like const.h #define LIVE

--- DTOperator/Const.cs
+++ DTOperator/Const.cs
@@ -10,8 +10,14 @@ namespace DTOperator
 	{
 		public static readonly String VERSION = "4.1:{git revision}_win";
 
-		public static readonly String USERSFILE = "C:\\Users\\A\\dtoperator\\users.conf";
-		public static readonly String CONFFILE = "C:\\Users\\A\\dtoperator\\dtoperator.conf";
+		private static readonly bool LIVE = false;
+		private static readonly String PREFIX_LIVE = @"C:\Program Files\dtoperator\";
+		private static readonly String PREFIX_KVM = @"C:\Users\A\dtoperator\";
+		private static readonly String PREFIX = LIVE ? PREFIX_LIVE : PREFIX_KVM;
+		public static readonly String USERSFILE = PREFIX + @"users.conf";
+		public static readonly String CONFFILE = PREFIX + @"dtoperator.conf";
+		public static readonly String LOGFOLDER = PREFIX + @"log\";
+
 		public static readonly int COMMANDSIZE = 2048;
 		public static readonly int MEDIASIZE = 1200;
 		public static readonly int MAXLISTENWAIT = 5;
@@ -26,8 +32,6 @@ namespace DTOperator
 
 		public static readonly int UNAUTHTIMEOUT = 500;
 		public static readonly int AUTHTIMEOUT = 3000;
-
-		public static readonly String LOGFOLDER = "C:\\Users\\A\\dtoperator\\log\\";
 
 		public static readonly int DEFAULTCMD = 1991;
 		public static readonly int DEFAULTMEDIA = 1961;
